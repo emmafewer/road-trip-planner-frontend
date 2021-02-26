a2762aa11ae1c9d1a879c84669c73ff3bf3b36ea
emmafewer <emmafewer20@gmail.com>
2021-02-25 20:21:06 -0800

    yay! I'm happy with my progress

--- src/components/ShowMap.js
+++ src/components/ShowMap.js
@@ -14,28 +14,76 @@ class ShowMap extends React.Component {
             center: [-95.7129, 37.0902],
             scrollZoom: true,
         });
+
         fetch("https://developer.nps.gov/api/v1/parks?&limit=1000&api_key=wrzMX2zd8xPlWQotxViQtACAPNmjfcmoylyVV7oR")
         .then(resp => resp.json())
         .then(parks => {
             this.specifyArea(parks, map)
-            // this.getParkMarkers(parks, map)
         })
     }
 
     specifyArea = (parks, map) => {
-        console.log(this.props.state.placesReducer.area)
-        let startLat = parks.data.filter(park => park.fullName === this.props.state.placesReducer.area.start)[0].latitude
-        let startLong = parks.data.filter(park => park.fullName === this.props.state.placesReducer.area.start)[0].longitude
+        let startLat = parseInt(parks.data.filter(park => park.fullName === this.props.state.placesReducer.area.start)[0].latitude)
+        let startLong = parseInt(parks.data.filter(park => park.fullName === this.props.state.placesReducer.area.start)[0].longitude)
 
-        let endLat = parks.data.filter(park => park.fullName === this.props.state.placesReducer.area.end)[0].latitude
-        let endLong = parks.data.filter(park => park.fullName === this.props.state.placesReducer.area.end)[0].longitude
+        let endLat = parseInt(parks.data.filter(park => park.fullName === this.props.state.placesReducer.area.end)[0].latitude)
+        let endLong = parseInt(parks.data.filter(park => park.fullName === this.props.state.placesReducer.area.end)[0].longitude)
 
-        let filter = parks.data.filter(park => park.latitude >= endLat && park.latitude <= startLat && park.longitude >= endLong && park.longitude <= startLong)
-        debugger
-        // Want to find lat and long of start and end then store in state.  
-        // Then we will filter places whose longitudes are between start and end 
-        // AND latitudes are between start and end
-        // Will need to find min and max
+        let lowLat = (Math.min(startLat, endLat)-3).toString()
+        let highLat = (Math.max(startLat, endLat)+3).toString()
+        let lowLong = (Math.min(startLong, endLong)-3).toString()
+        let highLong = (Math.max(startLong, endLong)+3).toString()
+
+        let filteredParks = parks.data.filter(park => park.latitude >= lowLat && park.latitude <= highLat && park.longitude <= lowLong && park.longitude >= highLong)
+        
+        const center = new mapboxgl.LngLat(((startLong+endLong)/2),((startLat+endLat)/2))
+        map.setCenter(center)
+        map.setZoom(5)
+        
+        this.getParkMarkers(filteredParks, map)
+    }
+
+    getParkMarkers = (parks, map) => {
+        const features = []
+
+        parks.forEach( park => {
+           
+            let feature = {
+                'type' : 'Feature',
+                'geometry' : {
+                    'type' : 'Point',
+                    'coordinates' : [ park.longitude, park.latitude ]
+                },
+                'properties' : {
+                    'name' : park.fullName,
+                    'description' : park.description,
+                    'image' : park.images
+                }
+            }
+            features.push(feature)
+        })
+
+        map.addSource("locations", {
+            "type": "geojson",
+            "data": {
+                "type": "FeatureCollection",
+                "features": features
+            }
+        })
+
+        features.forEach(marker => {
+            const popup = new mapboxgl.Popup()
+                .setHTML('<div style="padding:0.3rem 0.3rem 0;text-align:center;">'
+                + '<h2 style="font-size:16px;margin:0 0 0.3rem;">' + marker.properties.name + '</h2>'
+                + '<p style="font-size:12px;margin:0;">Description: ' + marker.properties.description + '</p></div>')
+          
+
+            new mapboxgl.Marker()
+                .setLngLat(marker.geometry.coordinates)
+                .setPopup(popup)
+                .addTo(map);
+        });
+
     }
 
     render () {
